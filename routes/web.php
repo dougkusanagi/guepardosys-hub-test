dc982498d3c1e188780a59116fe56e988f694298
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-07-02 16:59:22 -0300

    transfered routes and stubs directories

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,42 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductImageController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Route::get('/', function () {
+//     return inertia('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/dashboard', function () {
+//     return inertia('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', fn () => redirect()->route('product.index'))
+    ->name('home');
+
+Route::get('/dashboard', fn () => redirect()->route('product.index'))
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/product', ProductController::class);
+
+    Route::resource('/category', CategoryController::class);
+
+    Route::delete('/delete-image/{media_item}', [ProductImageController::class, 'deleteImage'])
+        ->name('image.delete');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
