72155dc7d7de7a51a83f5b185068d8610f74043b
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-07-02 17:20:23 -0300

    transfered  app.js, composer and npm dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,51 @@
-import './bootstrap';
-import '../css/app.css';
+import "../css/app.css";
+import "./bootstrap";
+import "vue-toastification/dist/index.css";
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { Head, Link, createInertiaApp, router } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import NProgress from "nprogress";
+import { createApp, h } from "vue";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
+import Toast from "vue-toastification";
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName =
+    window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
+
+router.on("start", () => NProgress.start());
+router.on("finish", () => NProgress.done());
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(Toast, {
+                position: "bottom-right",
+                timeout: 5000,
+                closeOnClick: true,
+                pauseOnFocusLoss: true,
+                pauseOnHover: true,
+                draggable: true,
+                draggablePercent: 0.6,
+                showCloseButtonOnHover: true,
+                hideProgressBar: false,
+                closeButton: "button",
+                icon: true,
+                rtl: false,
+            })
+            .component("Link", Link)
+            .component("Head", Head)
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: "#4338ca",
+        showSpinner: true,
     },
 });
